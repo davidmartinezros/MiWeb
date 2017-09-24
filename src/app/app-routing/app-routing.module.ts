2996d89f6a0e5d82441ec5b549d8f77382507962
David Martinez Ros <davidnezan@gmail.com>
2017-09-24 17:51:19 +0200

    implements a redux dispatch before any url load. implements a componentguard that define the canActivate of any url

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes, ɵROUTER_PROVIDERS } from '@angular/router';
+import { NgModule, Injectable }             from '@angular/core';
+import { RouterModule, Routes, CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { LocationStrategy, HashLocationStrategy, PathLocationStrategy } from '@angular/common';
 import { DashboardComponent }   from '../dashboard/dashboard.component';
@@ -8,25 +8,38 @@ import { FollowingComponent }   from '../following/following.component';
 import { ProjectDetailComponent }  from '../project-detail/project-detail.component';
 import { ProjectTypeComponent }  from '../project-type/project-type.component';
 
+@Injectable()
+export class ComponentGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+ 
+  public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    console.log(route);
+    console.log(state)
+    //this.router.navigate(["/"]);
+    return true;
+  }
+
+}
+
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'following', component: FollowingComponent },
-  { path: 'detail/:id', component: ProjectDetailComponent },
-  { path: 'type/:tipus', component: ProjectTypeComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full', canActivate: [ComponentGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [ComponentGuard] },
+  { path: 'projects', component: ProjectsComponent, canActivate: [ComponentGuard] },
+  { path: 'following', component: FollowingComponent, canActivate: [ComponentGuard] },
+  { path: 'detail/:id', component: ProjectDetailComponent, canActivate: [ComponentGuard] },
+  { path: 'type/:tipus', component: ProjectTypeComponent, canActivate: [ComponentGuard] }
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ],
   providers: [
-    ɵROUTER_PROVIDERS,
-    {provide: APP_BASE_HREF, useValue : '/' },
-    {provide: LocationStrategy, useClass: PathLocationStrategy  }
+    { provide: APP_BASE_HREF, useValue : '/' },
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
+    ComponentGuard
   ]
 })
-
 export class AppRoutingModule {
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule, forwardRef } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -20,6 +20,31 @@ import { ProjectComponent } from './project/project.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectTypeComponent } from './project-type/project-type.component';
 
+import { NgReduxModule, NgRedux } from 'ng2-redux';
+import {
+  applyMiddleware,
+  Store,
+  combineReducers,
+  compose,
+  createStore,
+} from 'redux';
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.text])
+    default:
+      return state
+  }
+}
+
+let store = createStore(todos, ['Use Redux'])
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'Read the web'
+})
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,6 +58,7 @@ import { ProjectTypeComponent } from './project-type/project-type.component';
     ProjectTypeComponent
   ],
   imports: [
+    NgReduxModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
@@ -47,4 +73,13 @@ import { ProjectTypeComponent } from './project-type/project-type.component';
     AppComponent
   ]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor() {
+
+    console.log(store.getState());
+    // [ 'Use Redux', 'Read the docs' ]
+
+  }
+
+}
