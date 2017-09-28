cdfcc0e7a11a8f790c9d573c1d9905c2dd506301
David Martinez Ros <davidnezan@gmail.com>
2017-09-28 21:11:12 +0200

    how to do to load the navigator language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,25 @@ import { TranslateService } from '@ngx-translate/core';
 
 export class AppComponent {
 
-  param = {value: 'world'};
+  private language: String;
   
   constructor(private translate: TranslateService) {
-    this.changeLanguage('en');
+    var userLang = navigator.language;
+    this.changeLanguage(userLang);
   }
 
   public changeLanguage(language) {
+
+    console.log(language);
+
     // this language will be used as a fallback when a translation isn't found in the current language
     this.translate.setDefaultLang(language);
     
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     this.translate.use(language);
+
+    this.language = language;
+    
   }
 
 
