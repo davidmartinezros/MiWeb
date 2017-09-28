e9adcdbeb3b5f2c2ba7d8dc6bfec12dde93ccfe1
David Martinez Ros <programador2.informatica@upf.edu>
2017-09-28 18:38:04 +0200

    implements the en language of the app.component.html page make the links and the typescript methods for changing between en and es

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,20 @@ export class AppComponent {
 
   param = {value: 'world'};
   
-  constructor(translate: TranslateService) {
+  constructor(private translate: TranslateService) {
       // this language will be used as a fallback when a translation isn't found in the current language
       translate.setDefaultLang('en');
 
         // the lang to use, if the lang isn't available, it will use the current loader to get them
       translate.use('en');
+  }
+
+  public changeLanguage(language) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    this.translate.setDefaultLang(language);
+    
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    this.translate.use(language);
   }
 
 
